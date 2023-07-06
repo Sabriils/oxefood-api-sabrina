bd7691f941ea7863267b5de57cf32bb601effc38
Aluno01 <Aluno01@gmail.com>
2023-07-06 20:19:19 -0300

    slide 25

---
+++ src/main/java/br/com/ifpe/oxefood/api/acesso/AuthenticationRequest.java
@@ -0,0 +1,23 @@
+package br.com.ifpe.oxefood.api.acesso;
+
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationRequest implements Serializable {
+
+    private static final long serialVersionUID = 7526932841380892557L;
+
+    private String username;
+
+    private String password;
+
+}
